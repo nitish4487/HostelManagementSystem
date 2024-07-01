6d0c3977f6420880c23d6c1656e3bb86ab36f862
nitish <nitish.k@avanseus.com>
2024-07-01 08:06:14 +0530

    changes related to mongodb string

--- module/hm.js
+++ module/hm.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 var mongoose = require("mongoose");
 
 // giving connection to mongoose
-const mongoURI = process.env.MONGODB_URI;
+MONGODB_URI="mongodb+srv://nitish448711:Nitish@31159@cluster0.dzgmclv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true ,useCreateIndex: true}).then(() => {
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true ,useCreateIndex: true}).then(() => {
     console.log('MongoDB connected successfully');
   }).catch(err => {
     console.error('MongoDB connection error:', err);
--- module/st.js
+++ module/st.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 // giving connection to mongoose
-const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true}).then(() => {
+// const mongoURI = process.env.MONGODB_URI;
+MONGODB_URI="mongodb+srv://nitish448711:Nitish@31159@cluster0.dzgmclv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true}).then(() => {
     console.log('MongoDB connected successfully');
   }).catch(err => {
     console.error('MongoDB connection error:', err);
